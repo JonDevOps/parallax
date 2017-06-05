233e1d3d9262ebec8948689bd7bb5098b6c35f9d
Heggy Castaneda <Heggy@Franks-MacBook-Pro.local>
2017-06-05 16:34:46 -0700

    added mouseMove function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 "use strict";
 
+function mouseMove (event) {
+    console.log(event.pageX, event.pageY);
+    
+}
 
-function mouseEventHandler() {
-console.log('window loaded');
-$('body').mousemove();
+function windowLoadHandler() {
+    console.log('window loaded');
+    $('body').mousemove(mouseMove); // mouseMove function getscalled
 }
 $(mouseEventHandler);
