37120bfb1ee36364945b1c79bb947debabfdf6a5
Heggy Castaneda <Heggy@Franks-MacBook-Pro.local>
2017-06-05 16:52:20 -0700

    centerMouse coordination

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 
 function mouseMove (event) {
     console.log(event.pageX, event.pageY);
-    
+    const windowWidth = $(window).width();
+    const windowHeight = $(window).height();
+    const windowCenter = {
+        left: windowWidth/2,
+        top: windowHeight/2 
+    };
+    const centeredMouse = {
+        left: event.pageX - windowCenter.left,
+        top: event.pageY - windowCenter.top
+    };
+    console.log(centeredMouse);
 }
+
 
 function windowLoadHandler() {
     console.log('window loaded');
     $('body').mousemove(mouseMove); // mouseMove function getscalled
 }
-$(mouseEventHandler);
+$(windowLoadHandler);
