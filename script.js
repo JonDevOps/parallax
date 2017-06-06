73e883ea18e87fae341873979718892ab4891d0d
Heggy Castaneda <Heggy@Franks-MacBook-Pro.local>
2017-06-05 17:46:09 -0700

    added new variable inside html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function mouseMove (event) {
-    console.log(event.pageX, event.pageY);
+    // console.log(event.pageX, event.pageY);
     const windowWidth = $(window).width();
     const windowHeight = $(window).height();
     const windowCenter = {
@@ -12,10 +12,14 @@ function mouseMove (event) {
         left: event.pageX - windowCenter.left,
         top: event.pageY - windowCenter.top
     };
-    console.log(centeredMouse);
+    // console.log(centeredMouse);
     $('#sideTiger>img').css(centeredMouse); // .css remembers its initial position from parents like anchor point
     $('#pika>img').css(centeredMouse);
     $('#tigerFront>img').css(centeredMouse);
+    $('#flower>img').css({
+        left: centeredMouse.left / $('#flower>img').data().scale,
+        top: centeredMouse.top / $('#flower>img').data().scale
+    }); // this is what is making it move anchor to oiriginal position
 }
 
 
